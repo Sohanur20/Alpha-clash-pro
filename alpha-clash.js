7e8888655bd9519f907300b56ccb1b4562154e6f
Sohanur20 <mdsohank168@gmail.com>
2024-02-14 23:45:08 +0600

    Get The Key Pressed And Check Right Or Wrong Key Pressed

--- alpha-clash.js
+++ alpha-clash.js
@@ -1,7 +1,7 @@
 
 
 // function play() {
-    
+
 // //Step 1 hide the home screen to hide the screen at the class hidden to the home section
 
 // const homeSection = document.getElementById('home-screen');
@@ -17,22 +17,46 @@
 // }
 
 function play() {
-    
+
     hideElementByid('home-screen')
     showElement('play-ground')
     continueGame()
 
 }
 
+function handleBtnPressKeyUp(event) {
+    const plyerKeyPresed = event.key;
+    console.log(' player key pressed', plyerKeyPresed)
+
+    const currentDisplyText = document.getElementById('display-text')
+    const currentAlphabetText = currentDisplyText.innerText;
+    const expectedAlphabt = currentAlphabetText.toLowerCase()
+
+    console.log(plyerKeyPresed, expectedAlphabt)
+
+
+    if (plyerKeyPresed === expectedAlphabt) {
+        console.log('you get a point')
+    }else{
+        console.log('you missed try again')
+    }
+}
+
+//cpature kybord kye press
+
+document.addEventListener('keyup', handleBtnPressKeyUp)
+
+
+
 function continueGame() {
-    
+
     const alphabet = getElementAlphabet();
-   console.log('random alphabet',alphabet)
+    console.log('random alphabet', alphabet)
 
-//    Set  randomly generated alphabet to the screen show it
+    //    Set  randomly generated alphabet to the screen show it
 
-const currentAlphabet = document.getElementById('display-text');
-currentAlphabet.innerText = alphabet ;
+    // const currentAlphabet = document.getElementById('display-text');
+    // currentAlphabet.innerText = alphabet;
 
-setBackgraundColor(alphabet)
+    // setBackgraundColor(alphabet)
 }
